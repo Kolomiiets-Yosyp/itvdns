4c28d803dad7d07a44a136eb46aa960531d77c48
Kolomiiets_Yosyp <Kolomiiets204@gmail.com>
2023-07-13 15:37:55 +0300

    validation

--- src/main/java/com/example/itvdn/ItvdnApplication.java
+++ src/main/java/com/example/itvdn/ItvdnApplication.java
@@ -3,7 +3,7 @@ package com.example.itvdn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.itvdn")
 public class ItvdnApplication {
 
     public static void main(String[] args) {
